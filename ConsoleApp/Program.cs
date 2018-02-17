8725e65a6972e0fe291e4d055544d769caa0e484
Yan Shlain <Yan.Shlain@gmail.com>
2018-02-17 21:18:08 +0200

    .

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,10 +9,10 @@ using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace ConsoleApp3
+namespace AwsEmulators.Demo.ConsoleApp
 {
 
-    internal enum SqsServiceProviders
+    public enum SqsServiceProviders
     {
         Undefined = 0,
         Amazon = 1,
@@ -31,27 +31,14 @@ namespace ConsoleApp3
         {
             try
             {
-                if (Directory.Exists(@"/Configuration"))
-                {
-                    string filePath = Path.Combine(@"/Configuration", "appsettings.json");
-                    if (File.Exists(filePath))
-                    {
-                        var content = File.ReadAllText(filePath);
-                    }
-                }
                 Configuration = ReadConfiguration("/Configuration", "appsettings.json");
+                var sqsClient = new SqsClient(Configuration, s_EmulatorName);
 
-                IAmazonSQS sqsClient = GetSqsClient(s_EmulatorName, Configuration);
+                var queues = await sqsClient.GetQueuesAsync();
+                string queueUrl = queues.FirstOrDefault(queueName => queueName == s_QueueName) ?? await sqsClient.CreateQueueAsync(s_QueueName);
 
-
-                var queues = await GetQueuesAsync(sqsClient);
-                string queueUrl = queues.FirstOrDefault(queueName => queueName == s_QueueName) ?? await CreateQueueAsync(sqsClient, s_QueueName);
-
-                //TODO: Remove below line.
-                queueUrl = queueUrl.Replace("/:/", "/172.25.39.209:4100/");
-
-                await SendMessageAsync(sqsClient, queueUrl);
-                await ReadMessagesAsync(sqsClient, queueUrl);
+                await sqsClient.SendMessageAsync(s_QueueName);
+                await sqsClient.ReadMessagesAsync(s_QueueName);
             }
             catch (Exception ex)
             {
@@ -66,80 +53,6 @@ namespace ConsoleApp3
                 .AddJsonFile(configurationFileName);
 
             return builder.Build();
-        }
-
-        private static async Task<string> CreateQueueAsync(IAmazonSQS sqsClient, string s_QueueName, CancellationToken cancellationToken = default)
-        {
-            var response = await sqsClient.CreateQueueAsync(s_QueueName, cancellationToken);
-            return response.QueueUrl;
-        }
-
-        private static IAmazonSQS GetSqsClient(SqsServiceProviders sqsProviderType, IConfiguration configuration)
-        {
-            IAmazonSQS sqsClient = null;
-
-            switch (sqsProviderType)
-            {
-                case SqsServiceProviders.Amazon:
-                    sqsClient = NativeAwsProvider.GetSqsClient();
-                    break;
-                case SqsServiceProviders.GoAws:
-                    sqsClient = new GoAws(configuration).GetSqsClient();
-                    break;
-                case SqsServiceProviders.LocalStack:
-                    sqsClient = new LocalStack(configuration).GetSqsClient();
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(sqsProviderType));
-            }
-
-            return sqsClient;
-        }
-
-        public static async Task<List<string>> GetQueuesAsync(IAmazonSQS sqsClient)
-        {
-            var response = await sqsClient.ListQueuesAsync(string.Empty);
-            return response.QueueUrls;
-        }
-
-        private static async Task SendMessageAsync(IAmazonSQS sqsClient, string queueUrl)
-        {
-            //Create the request to send
-            var sendRequest = new SendMessageRequest
-            {
-                QueueUrl = queueUrl,
-                MessageBody = "Curret local time is: " + DateTime.Now.ToLongDateString()
-            };
-
-            //Send the message to the queue and wait for the result
-            Console.WriteLine("Sending Message");
-            var sendMessageResponse = await sqsClient.SendMessageAsync(sendRequest);
-        }
-
-        private static async Task ReadMessagesAsync(IAmazonSQS amazonSQSClient, string queueUrl)
-        {
-            Console.WriteLine("Receiving Message");
-
-            //Create a receive requesdt to see if there are any messages on the queue
-            var receiveMessageRequest = new ReceiveMessageRequest();
-            receiveMessageRequest.QueueUrl = queueUrl;
-
-            //Send the receive request and wait for the response
-            var response = await amazonSQSClient.ReceiveMessageAsync(receiveMessageRequest);
-
-            //If we have any messages available
-            foreach (var message in response.Messages)
-            {
-                //Spit it out
-                Console.WriteLine(message.Body);
-
-                //Remove it from the queue as we don't want to see it again
-                var deleteMessageRequest = new DeleteMessageRequest();
-                deleteMessageRequest.QueueUrl = queueUrl;
-                deleteMessageRequest.ReceiptHandle = message.ReceiptHandle;
-
-                var result = amazonSQSClient.DeleteMessageAsync(deleteMessageRequest).Result;
-            }
         }
     }
 }
---
+++ ConsoleApp/SqsClient.cs
@@ -0,0 +1,110 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using ConsoleApp3;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AwsEmulators.Demo.ConsoleApp
+{
+    public class SqsClient
+    {
+        private readonly IConfiguration _configuration;
+        private readonly SqsServiceProviders _providerType;
+        private IAmazonSQS _sqsClient;
+
+        public IAmazonSQS Client
+        {
+            get
+            {
+                if (_sqsClient == null)
+                {
+                    this._sqsClient = GetSqsClient(_providerType, _configuration);
+                }
+
+                return _sqsClient;
+            }
+        }
+
+        public SqsClient(IConfiguration configuration, SqsServiceProviders providerType)
+        {
+
+            this._configuration = configuration;
+            this._providerType = providerType;
+        }
+
+        public async Task<string> CreateQueueAsync(string s_QueueName, CancellationToken cancellationToken = default)
+        {
+            var response = await Client.CreateQueueAsync(s_QueueName, cancellationToken);
+            return response.QueueUrl;
+        }
+
+        private static IAmazonSQS GetSqsClient(SqsServiceProviders sqsProviderType, IConfiguration configuration)
+        {
+            IAmazonSQS sqsClient = null;
+
+            switch (sqsProviderType)
+            {
+                case SqsServiceProviders.Amazon:
+                    sqsClient = NativeAwsProvider.GetSqsClient();
+                    break;
+                case SqsServiceProviders.GoAws:
+                    sqsClient = new GoAws(configuration).GetSqsClient();
+                    break;
+                case SqsServiceProviders.LocalStack:
+                    sqsClient = new LocalStack(configuration).GetSqsClient();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(sqsProviderType));
+            }
+
+            return sqsClient;
+        }
+
+        public async Task<List<string>> GetQueuesAsync()
+        {
+            var response = await Client.ListQueuesAsync(string.Empty);
+            return response.QueueUrls;
+        }
+
+        public async Task<SendMessageResponse> SendMessageAsync(string queueName)
+        {
+            //Create the request to send
+            var sendRequest = new SendMessageRequest
+            {
+                QueueUrl = $"{Client.Config.ServiceURL}/queues/{queueName}",
+                MessageBody = "Curret local time is: " + DateTime.Now.ToLongDateString()
+            };
+
+            var sendMessageResponse = await Client.SendMessageAsync(sendRequest);
+            return sendMessageResponse;
+        }
+
+        public async Task<ReceiveMessageResponse> ReadMessagesAsync(string queueName)
+        {
+            //Create a receive requesdt to see if there are any messages on the queue
+            var receiveMessageRequest = new ReceiveMessageRequest();
+            receiveMessageRequest.QueueUrl = $"{Client.Config.ServiceURL}/queues/{queueName}";
+
+            //Send the receive request and wait for the response
+            var response = await Client.ReceiveMessageAsync(receiveMessageRequest);
+            return response;
+        }
+
+        public async Task<DeleteMessageResponse> DeleteMessageFrowQueueAsync(string queueName, string receiptHandle)
+        {
+            //Remove it from the queue as we don't want to see it again
+            var deleteMessageRequest = new DeleteMessageRequest
+            {
+                QueueUrl = $"{Client.Config.ServiceURL}/queues/{queueName}",
+                ReceiptHandle = receiptHandle
+            };
+
+            var result = await Client.DeleteMessageAsync(deleteMessageRequest);
+            return result;
+        }
+    }
+}
